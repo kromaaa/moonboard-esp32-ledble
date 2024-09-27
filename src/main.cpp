5b665f8e1850cbd69d6d01baabbe929cbc86bb71
kroma <79542752+kromaaa@users.noreply.github.com>
2024-09-27 08:23:49 +0200

    fix comment

--- src/main.cpp
+++ src/main.cpp
@@ -457,7 +457,7 @@ void loop()
 
     if (bleConnected)
     {
-        # neoPixelReset(); should not reset problem if bluetooth disconnected
+        // # neoPixelReset(); should not reset problem if bluetooth disconnected
         while (bleSerial.available())
         {
             char c = bleSerial.read();
